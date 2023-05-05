73c58e3def7dbe92e2f7b1520fecf4e5ec0be2bf
Mamarkova-lanit <131162779+Mamarkova-lanit@users.noreply.github.com>
2023-05-05 21:21:13 +0300

    Задача 13

---
+++ DZ-10/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int num = ReadInt("Введите трехзначное число: ");
+
+
+int amount = num.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(num));
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int InCenter(int b)
+{
+    
+    int result = ((b / 10) % 10);
+    return result;
+}
