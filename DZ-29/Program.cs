79dfe08091a7315a083ce1d8744844bc97d54cfa
Mamarkova-lanit <131162779+Mamarkova-lanit@users.noreply.github.com>
2023-05-20 11:19:16 +0300

    задача 29

---
+++ DZ-29/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] masiv = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    Console.WriteLine($"Введите число {i}");
+    masiv[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+string s = String.Join(", ", masiv);
+ 
+        Console.WriteLine(s);       
